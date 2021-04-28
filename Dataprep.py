a957240d61eb4972b2beed289c4dc6707e8aec61
Geomodulation <chicagoswiss666@gmail.com>
2021-04-28 16:46:21 +0200

    Update Dataprep.py

--- Dataprep.py
+++ Dataprep.py
@@ -15,6 +15,14 @@ import openpyxl
 #1.2 read the excel file.
 df = pd.read_excel("D:\School\Master 2 Semester\Geodata Analysis and Modeling\Final Project\Excel Files\Test Excel Dataset.xlsx", sheet_name='Sheet1', engine= 'openpyxl')
 
+#1.3 Search Median income, divide by 100 then multiply by 80, to get 80% value. Then Dividd that value by 100 and multiply by 30.
+#this value is the maximum rent cost a property cna have to be noas.
+
+df.drop()
+
+#1.4 compare values generated at 1.3 with the colum with rent prices, and delete rows where rent exceeds the values of 1.3
+
+#1.5 return, save newly created excel file.
 
 
 #2.0map creation, probably with cartopy, if not maybe with contextily of geopandas
