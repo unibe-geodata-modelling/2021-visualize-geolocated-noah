cf5d7cb5cb93e94e5cf08bfa2a1fc5e8d1477c8b
Geomodulation <chicagoswiss666@gmail.com>
2021-09-27 16:02:30 +0200

    Update Dataprep.py

--- Dataprep.py
+++ Dataprep.py
@@ -17,10 +17,11 @@ import openpyxl
 # one open question is how the axis are defined, depending maybe also if I use cartopy or something els
 #1.1 remember import the stuff above so the programm is able to read the excel files easier. so 0.0
 #currently I have prepared a excel file which this program will access
-#1.2 read the excel file.
-myworkspace="C:/DATA/develops/2021-visualize-geolocated-noah"
+#1.2 read the excel file. To facilitate the fact that each end user will be creating their own, the location will be defined up top
+myworkspace="C:\FinnFeldmann\Excel Files"
 
-df = pd.read_excel(myworkspace+"/dfnew.xlsx", sheet_name='Sheet1', engine= 'openpyxl')
+df = pd.read_excel(myworkspace+"\Test Excel Dataset.xlsx", sheet_name='Sheet1', engine= 'openpyxl')
+
 
 #1.3 Search Median income, divide by 100 then multiply by 80, to get 80% value. Then Dividd that value by 100 and multiply by 30.(or multiply by.24)
 #this value is the maximum rent cost a property cna have to be noas.
