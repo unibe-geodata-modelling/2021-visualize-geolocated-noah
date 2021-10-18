de070cf76a0ffd9d74500867bdaca74dd6ebc058
Geomodulation <chicagoswiss666@gmail.com>
2021-10-18 19:02:41 +0200

    Update Dataprep.py

--- Dataprep.py
+++ Dataprep.py
@@ -17,53 +17,38 @@ import contextily as ctx
 #1.1 To facilitate the reading of the user input data, the location of the prepared excel files must be set. This must changed according to where the user has stored their prepared excel files.
 myworkspace="C:\FinnFeldmann\Excel Files"
 
-#1.2 This step reads the excel file.
+#1.2 This step reads the excel file. If the example file name or sheet name have been change, then this must be adapted here as well.
 
 df = pd.read_excel(myworkspace+"\Test Excel Dataset.xlsx", sheet_name='Sheet1', engine= 'openpyxl')
 
-
-#1.3 Search Median income, divide by 100 then multiply by 80, to get 80% value. Then Dividd that value by 100 and multiply by 30.(or multiply by.24)
-#this value is the maximum rent cost a property cna have to be noas.
+#1.3 Here the rentmax is defined, by multiplying the median income by .24, as per Finn Feldmanns NOAH formula, see also the read me
 
 df["rentmax"]= df['Median Income (Monthly)'] *0.24 #calculates the maximum rent for the median mean and adds it as a new column caller rentmanx to the df
 
-
-
-
 #1.4 compare values generated at 1.3 with the colum with rent prices, and delete rows where rent exceeds the values of 1.3
-dfnew= df[df['Rent Price (Monthly)']<=df["rentmax"]] #this part created new dataframe (dfnew) which as a condition only includes rows where rent montly is smaller or equalt to rentmax.
 
-#1.5 return, save newly created excel file.
+dfnew= df[df['Rent Price (Monthly)']<=df["rentmax"]] #this part creates a new dataframe (dfnew) which as a condition only includes rows where rent montly is smaller or equal to rentmax.
 
-#option 1
-dfnew.to_excel(myworkspace+"\dfnew.xlsx")# saves it but only in the reopsitory location, ideally I would like to specify where to save it$
-#optionally precreate a ecel file an write to sheet
+#1.5 save newly created NOAH only dataset to the second prepared Excel file Test NOAH Dataset
 
-#option 2 this variant you need to make an excel in advance and give path and sheet name. Thne the data will be written to the sheet of the preixisting excel file
+#option 1 (not recommended)
 
+dfnew.to_excel(myworkspace+"\dfnew.xlsx")# saves it but only in the reopsitory location,
+
+#option 2 (recommended and used for part 2) this variant you adapt the excel file Test NOAH Dataset according to the read me
 
 dfnew.to_excel(myworkspace+"\Test NOAH Dataset.xlsx", sheet_name="Page 1")
 
 
+#2.0map creation from the adapted excel file Test NOAH Dataset
+#2.1 In case part 2 is not performed right after part 1 (i.e. part 1 is no longer in memory) then part 0.0 must be run again
 
 
-#2.0map creation, probably with cartopy, if not maybe with contextily of geopandas
-#2.1 import things, that might need, here in addition to step 0.0.
-#2.1 I have decided to first try with contextily as the normal background map seemed better for my aplicatin than with cartopy
-#2.1 therefore these imports are taken from excercies introduction as I will do something similar
-import pip
-import fiona
-import pyproj
-import rtree
-import shapely
-import geopandas as gpd
-import contextily as ctx
-#2.2 First need to import the excel output of step 1.If step one was just performed then this file will still be in memory
-#2.2 If not the code below can be read. this is both if step 1 was done before this session, or if the file needent be prepared and could be used directly.pathway needs to be given.
-
+#2.2 In case part 2 is not performed right after part 1 (i.e. part 1 is no longer in memory) then the adapted excel file Test NOAH Dataset must be reloaded into memory, in which case the code below must be run
 
 dfnew = pd.read_excel(myworkspace+"\Test NOAH Dataset.xlsx", sheet_name="Page 1", engine= 'openpyxl')
-#2.3 current dataframe has no geometry, so need to convert it so it can have a crs.
+
+#2.3 current dataframe has no geometry, so need to convert it so it can have a crs (be attributed to a projection system)
 
 gdf = gpd.GeoDataFrame(dfnew, geometry=gpd.points_from_xy(dfnew.Longitude,dfnew.Latitude))
 gdf.crs
